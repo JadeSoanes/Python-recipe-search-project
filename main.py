573fd9bb06b216cbf8c22b1a7ffaa79a640b0d03
jadesoanes <soanesjl@gmail.com>
2022-08-20 15:21:54 +0100

    init push

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+def recipe_search(ingredient):
+    app_id = ''
+    app_key = ''
+    result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
+    data = result.json()
+    return data['hits']
+

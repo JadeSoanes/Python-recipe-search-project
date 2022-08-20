0d9c5fc6d8325189f0dc14f228bdf0c9ee4457b0
jadesoanes <soanesjl@gmail.com>
2022-08-20 15:31:05 +0100

    init commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 def recipe_search(ingredient):
-    app_id = ''
-    app_key = ''
+    app_id = '2ce04194'
+    app_key = '884c0755cdc7675a9bc6a788c0f4dcd1'
     result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
     data = result.json()
     return data['hits']
